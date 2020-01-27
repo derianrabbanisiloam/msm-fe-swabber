3c14dadf387f2ec55b02e62ecf8422ae645ce95a
johnpieter <john.tobing@siloamhospitals.com>
2020-01-27 18:00:04 +0700

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   CALL_CENTER_SERVICE: 'http://10.85.139.13/msm-be-call-center-master/api/v2',
   OPADMIN_SERVICE: 'http://10.85.139.13/msm-be-opadmin-master/api/v2',
   FRONT_OFFICE_SERVICE: 'http://10.85.139.13/msm-be-front-office-master/api/v2',
-  BPJS_SERVICE: '',
+  BPJS_SERVICE: 'test',
   SSO_SERVICE: 'http://10.85.129.55:7500',
   WEB_SOCKET_SERVICE: 'http://10.85.139.12:7779',
   HIS_SERVICE: 'http://10.85.139.13/his-service-master',

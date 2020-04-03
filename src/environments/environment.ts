efa3ea64ed35c5ddb1b07e3fd2275f2ef03ca419
johnpieter <john.tobing@siloamhospitals.com>
2020-04-03 13:20:23 +0700

    fix filter and table list view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,13 @@
-// export const environment = {
-//   production: false,
-//   ENVIRONMENT_NAME: 'Development',
-//   CALL_CENTER_SERVICE: 'http://10.85.139.13/msm-be-call-center-master/api/v2',
-//   OPADMIN_SERVICE: 'http://10.85.139.13/msm-be-opadmin-master/api/v2',
-//   FRONT_OFFICE_SERVICE: 'http://10.85.139.13/msm-be-front-office-master/api/v2',
-//   SSO_SERVICE: 'http://10.85.129.55:7500',
-//   WEB_SOCKET_SERVICE: 'http://10.85.139.12:8860',
-//   HIS_SERVICE: 'http://10.85.139.13/his-service-master',
-//   ASSET_PATH: 'https://gtn-dev-mysiloam-02.siloamhospitals.com/fo-assets/',
-//   UPLOAD_IMAGE: 'http://10.85.136.23:3333',
-//   GET_IMAGE_KTP: 'http://10.85.136.23/dev/identity/'
-// };
-
 export const environment = {
   production: false,
-  ENVIRONMENT_NAME: 'Testing (UAT)',
-  CALL_CENTER_SERVICE: 'http://10.85.139.13/msm-be-call-center-aido-prod/api/v2',
-  OPADMIN_SERVICE: 'http://10.85.139.13/opadmin/api/v2',
-  FRONT_OFFICE_SERVICE: 'http://10.85.139.13/msm-be-front-office-aido-prod/api/v2',
+  ENVIRONMENT_NAME: 'Development',
+  CALL_CENTER_SERVICE: 'http://10.85.139.13/msm-be-call-center-master/api/v2',
+  OPADMIN_SERVICE: 'http://10.85.139.13/msm-be-opadmin-master/api/v2',
+  FRONT_OFFICE_SERVICE: 'http://10.85.139.13/msm-be-front-office-master/api/v2',
   SSO_SERVICE: 'http://10.85.129.55:7500',
   WEB_SOCKET_SERVICE: 'http://10.85.139.12:8860',
-  HIS_SERVICE: 'http://10.85.139.13/his-get',
+  HIS_SERVICE: 'http://10.85.139.13/his-service-master',
   ASSET_PATH: 'https://gtn-dev-mysiloam-02.siloamhospitals.com/fo-assets/',
-  UPLOAD_IMAGE: 'http://10.85.136.23:6666',
-  GET_IMAGE_KTP: 'http://10.85.136.23/staging/identity/'
+  UPLOAD_IMAGE: 'http://10.85.136.23:3333',
+  GET_IMAGE_KTP: 'http://10.85.136.23/dev/identity/'
 };

7fc2a339622a8ecdcea9ddeacde04e1bad0507c4
johnpieter <john.tobing@siloamhospitals.com>
2020-12-01 11:32:54 +0700

    add view count in aido worklist

--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -79,6 +79,12 @@ export class AppointmentService {
     const url = `${this.ccAppointmentUrl}/history/${appointment}`;
     return this.http.get<any>(url, httpOptions);
   }
+
+  countAidoAppointment(hospital: string, fromDate: any, toDate: any): Observable<any> {
+    const url = `${this.ccAppointmentUrl}/aido/count?hospitalId=${hospital}&from=${fromDate}&to=${toDate}`;
+    return this.http.get<any>(url, httpOptions);
+  }
+
   getCountAppReschedule(hospital: string, channel?: string, exclude?: boolean) {
     let url = `${this.appointmentRescheduleCount}?hospitalId=${hospital}`;
     url = channel ? `${url}&channelId=${channel}` : url;
--- src/app/views/sections/section-sidebar/section-sidebar.component.ts
+++ src/app/views/sections/section-sidebar/section-sidebar.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { BpjsService } from '../../../services/bpjs.service';
+import { AppointmentService } from '../../../services/appointment.service';
 import socket from 'socket.io-client';
 import { SecretKey, Jwt, REQUEST_LIST, keySocket } from '../../../variables/common.variable';
 import Security from 'msm-kadapat';
 import { environment } from '../../../../environments/environment';
 import { hospitalId } from '../../../variables/common.variable';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-section-sidebar',
@@ -14,14 +16,17 @@ import { hospitalId } from '../../../variables/common.variable';
 export class SectionSidebarComponent implements OnInit {
   public assetPath = environment.ASSET_PATH;
   public countReqList: number = 0;
+  public countAidoList: number = 0;
   private socket;
   public key: any = JSON.parse(localStorage.getItem('key'));
   public hospital = this.key.hospital;
   public yogyaHospitalId = hospitalId.yogyakarta;
   public isBpjs = this.key.hospital.isBpjs;
+  public dateNow: any = moment().format('YYYY-MM-DD');
 
   constructor(
     private bpjsService: BpjsService,
+    private appointmentService: AppointmentService
   ) {
     this.socket = socket(`${environment.WEB_SOCKET_SERVICE + keySocket.BPJS}`, {
       transports: ['websocket'],
@@ -30,6 +35,7 @@ export class SectionSidebarComponent implements OnInit {
         }&url=${environment.BPJS_SERVICE}`,
     });
     this.countRechedule();
+    this.countAido();
   }
 
   ngOnInit() {
@@ -42,6 +48,14 @@ export class SectionSidebarComponent implements OnInit {
     this.bpjsService.getCountReqList(this.hospital.id).subscribe(
       data => {
         this.countReqList = data.data;
+      }
+    );
+  }
+
+  countAido() {
+    this.appointmentService.countAidoAppointment(this.hospital.id, this.dateNow, this.dateNow).subscribe(
+      data => {
+        this.countAidoList = data.data;
       }
     );
   }

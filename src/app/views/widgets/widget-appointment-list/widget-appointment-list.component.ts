67f9b8d101988c766b7d8ae50bea4e7bdec506ca
inggards <inggards@gmail.com>
2020-12-08 16:59:34 +0700

    fix bug button print eligibility

--- src/app/views/widgets/widget-appointment-list/widget-appointment-list.component.ts
+++ src/app/views/widgets/widget-appointment-list/widget-appointment-list.component.ts
@@ -1409,14 +1409,18 @@ export class WidgetAppointmentListComponent implements OnInit {
   }
 
   async printSjp(str){
-    this.isLoadingCheckEligible = true;
-    let filePdf = null
-    if (str === 'update'){
-      filePdf = await this.getFilePdfUpdate() 
+    if(this.patientType.description == 'PAYER'){
+      this.isLoadingCheckEligible = true;
+      let filePdf = null
+      if (str === 'update'){
+        filePdf = await this.getFilePdfUpdate() 
+      } else {
+        filePdf = await this.getFilePdf() 
+      }
+      printPreview(filePdf)
     } else {
-      filePdf = await this.getFilePdf() 
+      this.alertService.error('cannot print eligibility', false, 3000)
     }
-    printPreview(filePdf)
   }
 
  async updateAdmission(val, content){

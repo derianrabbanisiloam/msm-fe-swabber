5b957e8296bcaa6083902bde344ed6f4360e5eef
Ryan Adhi <ryanadhi.p@gmail.com>
2021-01-19 13:21:46 +0700

    consent model

---
+++ src/app/models/consents/consent.ts
@@ -0,0 +1,10 @@
+export class Consent {
+  consent_id: number;
+  organization_id: number;
+  patient_name: string;
+  date_of_birth: string;
+  mobile_no: string;
+  email_address: string;
+  create_user: string;
+  detail: any;
+}

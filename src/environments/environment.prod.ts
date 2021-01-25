382d8fead0e3c845a2ecd454ca2419034b25ea20
Ryan Adhi <ryan.adhi@bithealth.co.id>
2021-01-25 16:54:26 +0700

    env modification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   ENVIRONMENT_NAME: '',
   CALL_CENTER_SERVICE: 'http://gtn-mysiloam-api-02/callcenter/api/v2',
   OPADMIN_SERVICE: 'http://gtn-mysiloam-api-01/opadmin/api/v2',
-  FRONT_OFFICE_SERVICE: 'http://10.85.139.13/frontoffice/api/v2',
+  FRONT_OFFICE_SERVICE: 'http://gtn-mysiloam-api-02/frontoffice/api/v2',
   BPJS_SERVICE: 'http://10.85.136.18/bpjs/api/v2',
   SSO_SERVICE: 'http://10.85.129.91:8500',
   WEB_SOCKET_SERVICE: 'http://gtn-mysiloam-websocket-01:7779',
@@ -12,6 +12,5 @@ export const environment = {
   UPLOAD_IMAGE: 'http://10.85.136.23:9999',
   GET_IMAGE: 'http://10.85.136.23/prod',
   MOBILE_SERVICE: 'http://10.85.136.16/mobileinternal/api/v2',
-  INHEALTH: 'http://10.85.129.91:8803',
-  VACCINE_CONSENT_SERVICE: 'http://10.85.139.13/consent-covid'
+  INHEALTH: 'http://10.85.129.91:8803'
 };

05d768b2c013afa7abc3a64c9577f129393b696d
Ryan Adhi <70564966+ryanadhibit@users.noreply.github.com>
2021-01-27 15:02:09 +0700

    Fixing feature (#398)

--- src/app/views/widgets/widget-vaccine-consent-list/widget-vaccine-consent-list.component.ts
+++ src/app/views/widgets/widget-vaccine-consent-list/widget-vaccine-consent-list.component.ts
@@ -31,12 +31,14 @@ export class WidgetVaccineConsentListComponent implements OnInit {
   public showWaitMsg: boolean = false;
   public showNotFoundMsg: boolean = false;
   public showDetailConsent: boolean = false;
-  public updateStatus: string = "Initial";
+  public updateStatus: string = "initial";
   public separator: string = "<answer>";
   public choosedPatient: PatientHope;
   public doctorList: Doctor[];
   public doctorSelected: any;
   public isAdmissionCreated: boolean = false;
+  public createAdmissionStatus: string = "initial";
+  public isConsentDetailChanged: boolean = false;
 
   constructor(
     private doctorService: DoctorService,
@@ -61,7 +63,7 @@ export class WidgetVaccineConsentListComponent implements OnInit {
           top: document.body.scrollHeight,
           behavior: "smooth",
         });
-      }, 500)
+      }, 500);
     });
     this.doctorService.getListDoctor(this.key.hospital.id).subscribe((res) => {
       this.doctorList = res.data;
@@ -70,10 +72,11 @@ export class WidgetVaccineConsentListComponent implements OnInit {
 
   searchByCode() {
     const orgId = this.key.hospital.orgId;
-    this.updateStatus = "Initial";
+    this.updateStatus = "initial";
     this.consents = [];
     this.showWaitMsg = true;
     this.showNotFoundMsg = false;
+    this.isAdmissionCreated = false;
     this.consentService.getByCode(this.uniqueCode, orgId).subscribe(
       (res) => {
         if (res.status === "Success") {
@@ -98,13 +101,18 @@ export class WidgetVaccineConsentListComponent implements OnInit {
   }
 
   searchByNameDob() {
-    this.updateStatus = "Initial";
+    this.updateStatus = "initial";
     const orgId = this.key.hospital.orgId;
     this.consents = [];
     this.showWaitMsg = true;
     this.showNotFoundMsg = false;
+    this.isAdmissionCreated = false;
+    const date = `${this.dob[0] + this.dob[1]}`;
+    const month = `${this.dob[3] + this.dob[4]}`;
+    const year = `${this.dob[6] + this.dob[7] + this.dob[8] + this.dob[9]}`;
+    const formattedDob = `${month}-${date}-${year}`;
     this.consentService
-      .getByNameDob(this.patientName, this.dob, orgId)
+      .getByNameDob(this.patientName, formattedDob, orgId)
       .subscribe(
         (res) => {
           if (res.status === "Success") {
@@ -128,8 +136,14 @@ export class WidgetVaccineConsentListComponent implements OnInit {
       );
   }
 
-  goToDetail(payload: any) {
-    this.updateStatus = "Initial";
+  getDetailInformation(payload: any) {
+    if (
+      this.isAdmissionCreated &&
+      payload.consent_id === this.consentInfo.consent_id
+    )
+      return;
+    this.isConsentDetailChanged = false;
+    this.updateStatus = "initial";
     this.consentInfo = payload;
     this.showDetailConsent = false;
     this.consentService.getDetailAnswer(payload.consent_id).subscribe(
@@ -153,8 +167,34 @@ export class WidgetVaccineConsentListComponent implements OnInit {
     );
   }
 
+  goToDetail(payload: any) {
+    if (this.isConsentDetailChanged) {
+      Swal.fire({
+        position: "center",
+        type: "warning",
+        title: "Are you sure you want to leave this form?",
+        showConfirmButton: true,
+        confirmButtonText: "Leave",
+        showCancelButton: true,
+        text: "Changes you made will be lost",
+      }).then((res) => {
+        if (res.value) {
+          this.getDetailInformation(payload);
+        }
+      });
+    } else {
+      this.getDetailInformation(payload);
+    }
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   updateConsent() {
-    this.updateStatus = "Loading";
+    document.documentElement.style.overflow = "hidden";
+    this.updateStatus = "loading";
     const orgId = this.key.hospital.orgId;
     this.consentService
       .updateConsent({
@@ -166,16 +206,34 @@ export class WidgetVaccineConsentListComponent implements OnInit {
       })
       .subscribe(
         (res) => {
-          this.updateStatus = "Success";
+          document.documentElement.style.overflow = "auto";
+          this.isConsentDetailChanged = false;
+          this.updateStatus = "loaded";
+          Swal.fire({
+            position: "center",
+            type: "success",
+            title: "Vaccine consent form updated successfully",
+            showConfirmButton: false,
+            timer: 2000,
+          });
         },
         (err) => {
-          this.updateStatus = "Failed";
+          this.updateStatus = "loaded";
+          document.documentElement.style.overflow = "auto";
+          Swal.fire({
+            position: "center",
+            type: "error",
+            title: "Vaccine consent form failed to update",
+            showConfirmButton: false,
+            timer: 2000,
+          });
         }
       );
   }
 
   fieldsChange(event: any, id: number) {
-    this.updateStatus = "Initial";
+    this.isConsentDetailChanged = true;
+    this.updateStatus = "initial";
     const foundIndex = this.consentInfo.detail.findIndex(
       (item) => item.consent_detail_id === id
     );
@@ -184,39 +242,67 @@ export class WidgetVaccineConsentListComponent implements OnInit {
     this.consentInfo.detail[foundIndex].answer_value = event.target.value;
   }
 
-  getAlertClass() {
-    switch (this.updateStatus) {
-      case "Loading":
-        return "alert alert-warning";
-      case "Success":
-        return "alert alert-success";
-      case "Failed":
-        return "alert alert-fail";
-      default:
-        return "alert-none";
+  printForm(): void {
+    if (this.isConsentDetailChanged) {
+      Swal.fire({
+        position: "center",
+        type: "error",
+        title: "Please save the updated consent form before you print",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    } else {
+      document.execCommand("print");
     }
   }
 
-  printForm() {
-    document.execCommand("print");
+  handleformChange(): void {
+    this.isConsentDetailChanged = true;
   }
 
   goToPatientData() {
-    this.router.navigate(["/patient-data"]);
+    if (this.isConsentDetailChanged) {
+      Swal.fire({
+        position: "center",
+        type: "warning",
+        title: "Are you sure you want to leave this form?",
+        showConfirmButton: true,
+        confirmButtonText: "Leave",
+        showCancelButton: true,
+        text: "Changes you made will be lost",
+      }).then((res) => {
+        if (res.value) {
+          this.router.navigate(["/patient-data"]);
+        }
+      });
+    } else {
+      this.router.navigate(["/patient-data"]);
+    }
   }
+
   searchPatientHOPE(e?) {
-    const hospitalId = this.key.hospital.id;
-    const params = {
-      patientName: this.consentInfo.patient_name,
-      birthDate: `${this.consentInfo.date_of_birth.split("-")[2]}-${this.consentInfo.date_of_birth.split("-")[1]
-        }-${this.consentInfo.date_of_birth.split("-")[0]}`,
-      localMrNo: "",
-    };
-    const modalRef = this.modalService.open(ModalSearchPatientComponent, {
-      windowClass: "modal-searchPatient",
-      size: "lg",
-    });
-    modalRef.componentInstance.searchKeywords = { ...params, hospitalId };
+    if (this.isConsentDetailChanged) {
+      Swal.fire({
+        position: "center",
+        type: "error",
+        title: "Please save the updated consent form before admitting patient",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    } else {
+      const hospitalId = this.key.hospital.id;
+      const params = {
+        patientName: this.consentInfo.patient_name,
+        birthDate: `${this.consentInfo.date_of_birth.split("-")[2]}-${this.consentInfo.date_of_birth.split("-")[1]
+          }-${this.consentInfo.date_of_birth.split("-")[0]}`,
+        localMrNo: "",
+      };
+      const modalRef = this.modalService.open(ModalSearchPatientComponent, {
+        windowClass: "modal-searchPatient",
+        size: "lg",
+      });
+      modalRef.componentInstance.searchKeywords = { ...params, hospitalId };
+    }
   }
 
   setDoctor(item) {
@@ -238,34 +324,51 @@ export class WidgetVaccineConsentListComponent implements OnInit {
       create_user: "FO",
     };
 
-    this.consentService.createAdmissionVaccine(payloadHope).subscribe((res) => {
-      payloadCheckin.admission_id = res.data.ResultEntityId;
-      this.consentService
-        .checkinconsent(payloadCheckin)
-        .subscribe((checkedInRes) => {
-          this.isAdmissionCreated = true;
-          const foundIndex = this.consents.findIndex(
-            (x) => x.consent_id === this.consentInfo.consent_id
-          );
-          this.consents[foundIndex].checkin_date = moment().format(
-            "DD-MM-YYYY hh:mm"
-          );
-          this.consentInfo.checkin_date = moment().format("DD-MM-YYYY hh:mm");
-          Swal.fire({
-            position: "center",
-            type: "success",
-            title: "Admission successfully created",
-            showConfirmButton: false,
-            timer: 2000,
-          });
-          setTimeout(() => {
-            window.scrollTo({
-              left: 0,
-              top: 0,
-              behavior: "smooth",
+    this.createAdmissionStatus = "loading";
+    document.documentElement.style.overflow = "hidden";
+    this.consentService.createAdmissionVaccine(payloadHope).subscribe(
+      (res) => {
+        payloadCheckin.admission_id = res.data.ResultEntityId;
+        this.consentService
+          .checkinconsent(payloadCheckin)
+          .subscribe((checkedInRes) => {
+            this.isAdmissionCreated = true;
+            const foundIndex = this.consents.findIndex(
+              (x) => x.consent_id === this.consentInfo.consent_id
+            );
+            this.consents[foundIndex].checkin_date = moment().format(
+              "DD-MM-YYYY hh:mm"
+            );
+            this.consentInfo.checkin_date = moment().format("DD-MM-YYYY hh:mm");
+            this.createAdmissionStatus = "loaded";
+            document.documentElement.style.overflow = "auto";
+            Swal.fire({
+              position: "center",
+              type: "success",
+              title: "Admission successfully created",
+              showConfirmButton: false,
+              timer: 2000,
             });
-          }, 500)
+            setTimeout(() => {
+              window.scrollTo({
+                left: 0,
+                top: 0,
+                behavior: "smooth",
+              });
+            }, 500);
+          });
+      },
+      (err) => {
+        this.createAdmissionStatus = "loaded";
+        document.documentElement.style.overflow = "auto";
+        Swal.fire({
+          position: "center",
+          type: "error",
+          title: "Create admission failed, please try again later",
+          showConfirmButton: false,
+          timer: 2000,
         });
-    });
+      }
+    );
   }
 }

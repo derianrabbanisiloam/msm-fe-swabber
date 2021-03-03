f1097fae0a405a1a0f1a9a6911a0c4d4e4a1bfdf
Ryan Adhi <ryan.adhi@bithealth.co.id>
2021-03-03 18:50:37 +0700

    lint page vaccine list module

--- src/app/views/pages/page-vaccine-consent-list/page-vaccine-consent-list.module.ts
+++ src/app/views/pages/page-vaccine-consent-list/page-vaccine-consent-list.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { PageVaccineConsentListRoutingModule } from "./page-vaccine-consent-list-routing.module";
-import { PageVaccineConsentListComponent } from "./page-vaccine-consent-list.component";
-import { WidgetVaccineConsentListComponent } from "../../widgets/widget-vaccine-consent-list/widget-vaccine-consent-list.component";
-import { SectionHeaderModule } from "../../sections/section-header/section-header.module";
-import { SectionSidebarModule } from "../../sections/section-sidebar/section-sidebar.module";
-import { SectionFooterModule } from "../../sections/section-footer/section-footer.module";
-import { ModalSearchPatientModule } from "../../widgets/modal-search-patient/modal-search-patient.module";
-import { ModalSearchPatientComponent } from "../../widgets/modal-search-patient/modal-search-patient.component";
-import { TextMaskModule } from "angular2-text-mask";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PageVaccineConsentListRoutingModule } from './page-vaccine-consent-list-routing.module';
+import { PageVaccineConsentListComponent } from './page-vaccine-consent-list.component';
+import { WidgetVaccineConsentListComponent } from '../../widgets/widget-vaccine-consent-list/widget-vaccine-consent-list.component';
+import { SectionHeaderModule } from '../../sections/section-header/section-header.module';
+import { SectionSidebarModule } from '../../sections/section-sidebar/section-sidebar.module';
+import { SectionFooterModule } from '../../sections/section-footer/section-footer.module';
+import { ModalSearchPatientModule } from '../../widgets/modal-search-patient/modal-search-patient.module';
+import { ModalSearchPatientComponent } from '../../widgets/modal-search-patient/modal-search-patient.component';
+import { TextMaskModule } from 'angular2-text-mask';
 import {
   NgbProgressbarModule,
   NgbTooltipModule,
   NgbTabsetModule,
   NgbModule,
   NgbModalModule,
-} from "@ng-bootstrap/ng-bootstrap";
+} from '@ng-bootstrap/ng-bootstrap';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { NguiAutoCompleteModule } from '@ngui/auto-complete';
 

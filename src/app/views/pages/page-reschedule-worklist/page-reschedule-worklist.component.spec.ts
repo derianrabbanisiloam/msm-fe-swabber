4d6e356ac824324ddf47db09663ad959613c31f4
Ricky .D <ricky.darsono@bithealth.co.id>
2021-06-29 13:09:57 +0700

    Page Reschedule - get test working - use widget module

--- src/app/views/pages/page-reschedule-worklist/page-reschedule-worklist.component.spec.ts
+++ src/app/views/pages/page-reschedule-worklist/page-reschedule-worklist.component.spec.ts
@@ -1,14 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageRescheduleWorklistComponent } from './page-reschedule-worklist.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {PageRescheduleWorklistModule} from './page-reschedule-worklist.module';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {RescheduleWorklistInterceptor} from '../../../interceptors/reschedule-worklist-interceptor';
+import {DoctorInterceptor} from '../../../interceptors/doctor-interceptor';
+import {mockLocalStorage} from '../page-vaccine-worklist/page-vaccine-worklist.component.spec';
 
 describe('PageRescheduleWorklistComponent', () => {
   let component: PageRescheduleWorklistComponent;
   let fixture: ComponentFixture<PageRescheduleWorklistComponent>;
 
   beforeEach(async(() => {
+    mockLocalStorage();
     TestBed.configureTestingModule({
-      declarations: [ PageRescheduleWorklistComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        PageRescheduleWorklistModule
+      ],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: RescheduleWorklistInterceptor,
+          multi: true,
+        },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: DoctorInterceptor,
+          multi: true,
+        },
+      ],
     })
     .compileComponents();
   }));
